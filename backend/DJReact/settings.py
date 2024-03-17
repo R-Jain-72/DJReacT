305c8dae931951e6d20ee9cecfd0c663277f2446
Rishabh J <rishabhj1470@gmail.com>
2024-03-17 13:49:22 +0530

    environment variables are set

--- backend/DJReact/settings.py
+++ backend/DJReact/settings.py
@@ -11,20 +11,22 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
-
+import environ
+env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = this security key is kept in .env so that it is not being pushed in github
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = this is also in .env
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
