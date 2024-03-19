319326e95ec74ad22bb0da2bac91d7c91cf904b3
Rishabh J <rishabhj1470@gmail.com>
2024-03-19 23:36:31 +0530

    custom models ongoing

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
 # Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_('First Name'), max_length=100)
+    last_name = models.CharField(_("Last Name"), max_length=100)
+    
